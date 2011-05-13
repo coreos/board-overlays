fc2fdea284b19dff2304e7fd48d3098bfc63fe0e
Hung-Te Lin <hungte@chromium.org>
2011-05-13 20:06:33 +0800

    chromeos-firmware-tegra2: enable customization (legacy upgrade and force RO+RW update)
    Add firmware update customization so that...
     - convert from an legacy firmware, even within autoupdate process
     - force upgrade RO+RW in recovery/autoupdate, to temporarily work on ARM.
    
    We will need to see how ARM firmware update plan changes and decide how to
    modify the customization.
    
    BUG=chromium-os:12194,chromium-os:15248
    TEST=emerge-tegra2_XXX chromeos-firmware-tegra2
         execute on ARM (with legacy firmware): seeing autoupdate=factory_install
         execute on ARM (with updated firmware): seeing incompatible update (RO+RW)
    
    Change-Id: Id0b6ed90b462cb95d54948f766cbf851974d275e
    Reviewed-on: http://gerrit.chromium.org/gerrit/978
    Reviewed-by: Tom Wai-Hong Tam <waihong@chromium.org>
    Tested-by: Hung-Te Lin <hungte@chromium.org>

---
+++ overlay-tegra2/chromeos-base/chromeos-firmware-tegra2/files/extra/updater_custom.sh
@@ -0,0 +1,37 @@
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# TARGET_PLATFORM="tegra2"
+
+CUSTOMIZATION_RW_COMPATIBLE_CHECK="updater_custom_rw_compatible_check"
+
+updater_custom_main() {
+  debug_msg "Customization for $TARGET_PLATFORM loaded."
+}
+
+updater_custom_rw_compatible_check() {
+  # Returns $FLAGS_FALSE if not compatible (i.e., need incompatible_mode)
+
+  if [ "${FLAGS_update_main}" = ${FLAGS_TRUE} ]; then
+    case "$FWID" in
+      * )
+        # For ARM platforms, there is almost nothing to change in RW yet;
+        # let's convert every 'autoupdate' request into 'recovery.
+        # We need to fix this one the new design of ARM firmware updating is
+        # complete.
+        VERSION=$(echo $(cat VERSION | grep BIOS\ version | cut -f2 -d:))
+        if [ "$VERSION" != "$FWID" ]; then
+          return $FLAGS_FALSE
+        else
+          true
+        fi
+    esac
+  fi
+}
+
+# One-time shot update for legacy firmware
+if ! crossystem hwid >/dev/null 2>&1; then
+  verbose_msg "One-time update from legacy firmware."
+  FLAGS_mode=factory_install
+fi
