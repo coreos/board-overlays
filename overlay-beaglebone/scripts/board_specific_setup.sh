80107b6ddc2d9b2adb0079b982f5095c1aa60718
Liam McLoughlin <lmcloughlin@chromium.org>
2012-08-22 17:34:06 -0700

    Add hybrid MBR creation to board specific setup scripts
    BUG=none
    TEST=Build Beaglebone image, verify it boots correctly
    
    Change-Id: I1d8e747470c68f47f1ec078a0b4f7834b4a52448
    Reviewed-on: https://gerrit.chromium.org/gerrit/31184
    Tested-by: Liam McLoughlin <lmcloughlin@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Ready: Liam McLoughlin <lmcloughlin@chromium.org>

--- overlay-beaglebone/scripts/board_specific_setup.sh
+++ overlay-beaglebone/scripts/board_specific_setup.sh
@@ -4,7 +4,25 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-board_setup() {
+# Creates a hybrid MBR which points the MBR partition 1 to GPT
+# partition 12 (ESP).
+install_hybrid_mbr() {
+  info "Creating hybrid MBR"
+  locate_gpt
+  local start_esp=$(partoffset "$1" 12)
+  local num_esp_sectors=$(partsize "$1" 12)
+  sudo sfdisk "$1" <<EOF
+unit: sectors
+
+disk1 : start=   ${start_esp}, size=    ${num_esp_sectors}, Id= c, bootable
+disk2 : start=   1, size=    1, Id= ee
+EOF
+}
+
+# Downloads and installs the MLO and copies u-boot to the ESP.
+# This is used combined with a hybrid MBR to allow booting
+# a Beaglebone from microSD only.
+install_beaglebone_bootloader() {
   local efi_offset_sectors=$(partoffset "$1" 12)
   local efi_size_sectors=$(partsize "$1" 12)
   local efi_offset=$(( efi_offset_sectors * 512 ))
@@ -18,6 +36,11 @@ board_setup() {
 
   sudo umount "$efi_dir"
   rmdir "$efi_dir"
+}
+
+board_setup() {
+  install_beaglebone_bootloader "$1"
+  install_hybrid_mbr "$1"
 }
 
 skip_blacklist_check=1
