cc5eb95c5c6bc6748484f188c3eb71522f3583aa
Benson Leung <bleung@chromium.org>
2012-12-17 16:21:04 -0800

    chromeos-touch-firmwareupdate.sh : Use cleaned up version of touch updater.
    Use a cleaned up version of this script that fixes a lot of style problems
    and some exit issues in the original script.
    
    Signed-off-by: Benson Leung <bleung@chromium.org>
    
    BUG=chrome-os-partner:16879
    TEST=Check failure recovery :
    run /opt/google/touchpad/firmware/chromeos-touch-firmwareupdate.sh -f -d cyapa
    While it is running, power down the system.
    On next boot, the system should go through a recovery firmware update,
    additional 17 seconds, and then have a functional trackpad on 2.4 fw.
    To check, cat /sys/bus/i2c/devices/1-0067/firmware_version.
    
    Change-Id: I8594cad1d4e2a8a647fb321229e20700669d2d0a
    Reviewed-on: https://gerrit.chromium.org/gerrit/39841
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Commit-Ready: Benson Leung <bleung@chromium.org>
    Tested-by: Benson Leung <bleung@chromium.org>

--- overlay-daisy/chromeos-base/chromeos-touch-firmware-daisy/files/chromeos-touch-firmwareupdate.sh
+++ overlay-daisy/chromeos-base/chromeos-touch-firmware-daisy/files/chromeos-touch-firmwareupdate.sh
@@ -6,126 +6,126 @@
 
 . /usr/share/misc/shflags
 
-DEFINE_boolean 'force' $FLAGS_FALSE "Force update" 'f'
-DEFINE_boolean 'recovery' $FLAGS_FALSE "Recovery. Allows for rollback" 'r'
+DEFINE_boolean 'force' ${FLAGS_FALSE} "Force update" 'f'
+DEFINE_boolean 'recovery' ${FLAGS_FALSE} "Recovery. Allows for rollback" 'r'
 DEFINE_string 'device' '' "device name" 'd'
 DEFINE_string 'firmware_name' '' "firmware name (in /lib/firmware)" 'n'
 
 log_msg() {
-    logger -t "chromeos-touch-firmwareupdate[${PPID}]" "$@"
-    echo "$@"
+  logger -t "chromeos-touch-firmwareupdate[${PPID}]" "$@"
+  echo "$@"
+}
+
+die() {
+  log_msg "error: $*"
+  exit 1
 }
 
 # Parse command line
 FLAGS "$@" || exit 1
-eval set -- "$FLAGS_ARGV"
+eval set -- "${FLAGS_ARGV}"
 
-if [ -z "$FLAGS_device" ]; then
-    log_msg "Please specify a device using -d"
-    return 1
+if [ -z "${FLAGS_device}" ]; then
+  die "Please specify a device using -d"
 fi
 
-trackpad_device_name="$FLAGS_device"
+trackpad_device_name="${FLAGS_device}"
 i2c_devices_path="/sys/bus/i2c/devices"
 trackpad_path=""
-update_needed=0
+update_needed=${FLAGS_FALSE}
 
-#find trackpad path in i2c devices
-for dev in $(ls $i2c_devices_path); do
-    dev_name=$(cat $i2c_devices_path/$dev/name)
-    if [ -n "$dev_name" ] &&
-       [ "$trackpad_device_name" = "$dev_name" ]; then
-        trackpad_path="$i2c_devices_path/$dev"
-        break
-    fi
+# Find trackpad path in i2c devices.
+for dev in "${i2c_devices_path}"/*/name; do
+  dev_name=$(cat "${dev}")
+  if [ "${trackpad_device_name}" = "${dev_name}" ]; then
+    trackpad_path="${dev%/name}"
+    break
+  fi
 done
-if [ -z "$trackpad_path" ]; then
-    log_msg "$trackpad_device_name not found on system. Aborting update."
-    return 1
+if [ -z "${trackpad_path}" ]; then
+  die "${trackpad_device_name} not found on system. Aborting update."
 fi
 
-if [ -z "$FLAGS_firmware_name" ]; then
-    fw_link_name="$trackpad_device_name.bin"
-else
-    fw_link_name="$FLAGS_firmware_name"
-fi
-fw_link_path="/lib/firmware/$fw_link_name"
+fw_link_name=${FLAGS_firmware_name:-${trackpad_device_name}.bin}
+case ${fw_link_name} in
+/*) fw_link_path=${fw_link_name} ;;
+*)  fw_link_path="/lib/firmware/${fw_link_name}" ;;
+esac
 
 update_firmware() {
-    echo -n 1 > $trackpad_path/update_fw
+  printf 1 > "$1/update_fw"
+  local ret=$?
+  if [ ${ret} -ne 0 ]; then
+    die "Error updating touch firmware. ${ret}"
+  fi
 }
 
 get_active_firmware_version() {
-    active_fw_version=$(cat $trackpad_path/firmware_version)
-    active_fw_version_major=${active_fw_version%%.*}
-    active_fw_version_minor=${active_fw_version##*.}
+  active_fw_version=$(cat "$1/firmware_version")
+  active_fw_version_major=${active_fw_version%.*}
+  active_fw_version_minor=${active_fw_version#*.}
 }
 
-fw_path=$(readlink "$fw_link_path")
-if [ ! -e "$fw_link_path" ] ||
-   [ ! -e "$fw_path" ]; then
-    log_msg "No valid firmware for $trackpad_device_name found."
-    return 1
+fw_path=$(readlink "${fw_link_path}")
+if [ ! -e "${fw_link_path}" ]; then
+  die "No valid firmware for ${trackpad_device_name} found."
 fi
-fw_filename=$(basename $fw_path)
-fw_name=${fw_filename%%.bin}
-product_id=${fw_name%%_*}
+fw_filename=${fw_path##*/}
+fw_name=${fw_filename%.bin}
+product_id=${fw_name%_*}
 
-fw_version=${fw_name##"${product_id}_"}
-fw_version_major=${fw_version%%.*}
-fw_version_minor=${fw_version##*.}
-active_product_id=$(cat $trackpad_path/product_id)
+fw_version=${fw_name#"${product_id}_"}
+fw_version_major=${fw_version%.*}
+fw_version_minor=${fw_version#*.}
+active_product_id=$(cat "${trackpad_path}/product_id")
 
-if [ -z "$active_product_id" ]; then
-    log_msg "Trackpad in non operational state. Updating."
-    update_needed=1
+if [ -z "${active_product_id}" ]; then
+  log_msg "Trackpad in non operational state. Updating."
+  update_needed=${FLAGS_TRUE}
 fi
 
-if [ -n "$active_product_id" ] &&
-   [ "$product_id" != "$active_product_id" ]; then
-    log_msg "Touch firmware updater: Product ID mismatch!"
-    log_msg "Hardware product id : $active_product_id"
-    log_msg "Updater product id  : $product_id"
-    return 1
+if [ -n "${active_product_id}" ] &&
+   [ "${product_id}" != "${active_product_id}" ]; then
+  log_msg "Hardware product id : ${active_product_id}"
+  log_msg "Updater product id  : ${product_id}"
+  die "Touch firmware updater: Product ID mismatch!"
 fi
 
-get_active_firmware_version
+get_active_firmware_version "${trackpad_path}"
 
-log_msg "Product ID : $product_id"
-log_msg "Current Firmware: $active_fw_version"
-log_msg "Updater Firmware: $fw_version"
+log_msg "Product ID : ${product_id}"
+log_msg "Current Firmware: ${active_fw_version}"
+log_msg "Updater Firmware: ${fw_version}"
 
-if [ "$active_fw_version_major" -lt "$fw_version_major" ] ||
-     ([ "$active_fw_version_major" -eq "$fw_version_major" ] &&
-      [ "$active_fw_version_minor" -lt "$fw_version_minor" ]); then
-    log_msg "Update needed."
-    update_needed=1
+if [ ${active_fw_version_major} -lt ${fw_version_major} ] ||
+     ([ ${active_fw_version_major} -eq ${fw_version_major} ] &&
+      [ ${active_fw_version_minor} -lt ${fw_version_minor} ]); then
+  log_msg "Update needed."
+  update_needed=${FLAGS_TRUE}
+elif [ ${active_fw_version_major} -eq ${fw_version_major} ] &&
+     [ ${active_fw_version_minor} -eq ${fw_version_minor} ]; then
+  log_msg "Firmware up to date."
+elif [ ${FLAGS_recovery} -eq ${FLAGS_TRUE} ]; then
+  log_msg "Recovery firmware update. Rolling back to ${fw_version}."
+  update_needed=${FLAGS_TRUE}
+else
+  log_msg "Current firmware ahead of updater. Use --recovery to downgrade."
 fi
 
-if [ "$active_fw_version_major" -eq "$fw_version_major" ] &&
-   [ "$active_fw_version_minor" -eq "$fw_version_minor" ]; then
-    log_msg "Firmware up to date."
-elif [ $FLAGS_recovery -eq $FLAGS_TRUE ] &&
-     [ $update_needed -eq 0 ]; then
-    log_msg "Recovery firmware update. Rolling back to $fw_version."
-    update_needed=1
+if [ ${FLAGS_force} -eq ${FLAGS_TRUE} ]; then
+  log_msg "Forcing update."
 fi
 
-if [ $FLAGS_force -eq $FLAGS_TRUE ]; then
-    log_msg "Forcing update."
+if [ ${FLAGS_force} -eq ${FLAGS_TRUE} ] ||
+   [ ${update_needed} -eq  ${FLAGS_TRUE} ]; then
+  log_msg "Update FW to ${fw_name}"
+  update_firmware "${trackpad_path}"
+  get_active_firmware_version "${trackpad_path}"
+  if [ ${active_fw_version_major} -ne ${fw_version_major} ] ||
+     [ ${active_fw_version_minor} -ne ${fw_version_minor} ]; then
+    die "Firmware update failed."
+  fi
+  log_msg "Update FW succeded. Current Firmware: ${active_fw_version}"
 fi
 
-if [ $FLAGS_force -eq $FLAGS_TRUE ] ||
-   [ $update_needed -eq 1 ]; then
-    log_msg "Update FW to $fw_name"
-    update_firmware
-    get_active_firmware_version
-    if [ "$active_fw_version_major" -ne "$fw_version_major" ] ||
-       [ "$active_fw_version_minor" -ne "$fw_version_minor" ]; then
-        log_msg "Firmware update failed."
-        return 1
-    fi
-    log_msg "Update FW succeded. Current Firmware: $active_fw_version"
-fi
-
-return 0
+exit 0
